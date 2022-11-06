a0c722a64eef3b29653c1d3bc78caced542e52f5
Krzysztof Bochenko <115269675+Krristoff@users.noreply.github.com>
2022-11-06 11:49:32 +0100

    Add files via upload
    Zoptaymalizowana gra

--- guess_game.py
+++ guess_game.py
@@ -50,44 +50,34 @@ import random
 #             break
 #     print("you guessed it!")
 TIMES_TO_PLAY = 10
-a = list() #przypisujemy pustą liste zmiennej a 99
-for i in range(TIMES_TO_PLAY):  
-    a.append(random.randint(1, 99))#Nadawanie na koniec listy "a" losowo utweorznej liczby o wartości od 1-99, wykonana 10 razy.
+def generate_random_numbers_list(maxvalue):
+    random_numbers_list = []
+    for i in range(TIMES_TO_PLAY):  
+        random_numbers_list.append(random.randint(1, maxvalue))
+    return random_numbers_list
 
 def ask_for_input(maxvalue):
     user_number=int(input("Enter an integer from 1 to {maxvalue}:"))
     return user_number
 
-
-for i in range(10): #otwieramy pętle, która będzie powtarzała dla i w przedziale 0-9
-    user_input=None
-    user_input = ask_for_input(99)   #pobieramy wartość, rzutujemy wartość zmiennej na typ integer i przypisujemy zmienną user_input
-    while a[i] != user_input:    #rozpoczynamy pętle, któa trwa, dopóki element i z listy a jest różny od user_input
-        if user_input < a[i]:    #sprawdza czy wartość pobrana od użytkownika jest mniejsza od kolejnego elementu z listy a  
-            print("guess is low")   #wyświetla powiadomienia z konsoli, że liczba jest za mała
-            
-        elif user_input > a[i]:#w przedziwnym wypadku, sprawdza czy wartość pobrana od użytkownika jest większa od kolejnego elementu z listy a 
-            print("guess is high")#wyświetla powiadomienia z konsoli, że liczba jest za duża
-        user_input = ask_for_input(99)#pobieramy wartość, rzutujemy wartość zmiennej na typ integer i przypisujemy zmienną user_input
- 
+def game_logic(maxvalue, random_number_list):
+    for i in range(TIMES_TO_PLAY):
+        user_input=None
+    while random_number_list[i] != user_input:
+        user_input=ask_for_input(maxvalue)
         
-    print("you guessed it!")#wyświetla w koncoli że zgadłeś90
-
-
-b = []
-for i in range(TIMES_TO_PLAY):
-    b.append(random.randint(1, 49))
-
-for i in range(10):
-    user_input=None
-    while b[i] != user_input:
-        user_input=ask_for_input(49)
-        if user_input < b[i]:
+        if user_input < random_number_list[i]:
             print("guess is low")
             
-        elif user_input > b[i]:
+        elif user_input > random_number_list[i]:
             print("guess is high")
-        
-        else:
-            break
     print("you guessed it!")
+
+a=generate_random_numbers_list(99)
+    
+game_logic(99,a)
+
+
+b=generate_random_numbers_list(49)
+
+game_logic(49,a)
